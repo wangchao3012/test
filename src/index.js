42b63d037fca5d4203f8250d559a62bc52a8d805
wangchao3012 <wangchao3012@163.com>
2016-12-19 23:47:37 +0800

    --

--- src/index.js
+++ src/index.js
@@ -1,17 +1,41 @@
 'use strict'
 
 var config = require('config');
+// var Sequelize = require('sequelize');
+// var db = {
+//     sequelize: new Sequelize(config.mysql.dbname, config.mysql.user, config.mysql.password, config.mysql.options)
+// }; 
+
+
 var Sequelize = require('sequelize');
-var db = {
-    sequelize: new Sequelize(config.mysql.dbname, config.mysql.user, config.mysql.password, config.mysql.options)
-};
-db.User = db.sequelize.import('../model/user.js');
-module.exports = db;
+var db = new Sequelize(config.mysql.dbname, config.mysql.user, config.mysql.password, config.mysql.options);
+
+db.User = db.import('./model/user.js');
+// db.Role = db.import('./model/role.js');
+db.sync({ force: false }).then(function () {
+    console.log("服务启动成功");
+}).catch(function (err) {
+    console.log("服务启动失败: %s", err);
+});
+
+
+// module.exports = db;
 
 const Koa = require('koa');
 const app = new Koa();
 
 app.use(ctx => {
-    ctx.body = '添加用户';
+    var time = new Date().getTime();
+    for (var i = 0; i < 10000; i++) {
+        var dmu = {
+            name: '张三' + new Date().getTime(),
+            age: 11,
+        };
+        db.User.create(dmu).then(function(user){
+
+    ctx.body = (new Date().getTime() - time) + 'ms 添加用户' + JSON.stringify(dmu);
+        });
+    }
+
 });
 app.listen(config.get('port'));
